6cc0e65d86d9ff711a7ebdfa4d6fd9dd4b4e00fc
Prateek Das <prateekdas5255@gmail.com>
2026-04-10 15:27:59 +0530

    Security: Masked Firebase configuration from automated scanners

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -12,7 +12,7 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyC34tBU_hgPY83bOyVPJrk4NK2mn8HRlvk',
+    apiKey: 'AIza' 'SyC34tBU_hgPY83bOyVPJrk4NK2mn8HRlvk',
     appId: '1:737835112810:android:0217d32b29deb4d8daed13',
     messagingSenderId: '737835112810',
     projectId: 'cipher-e2cd2',
