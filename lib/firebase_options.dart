7653204e22ddc10b8ff11010a87c1a80de95abbd
Prateek Das <prateekdas5255@gmail.com>
2026-04-10 16:08:21 +0530

    Security: Fully obfuscated Firebase credentials with runtime decoding

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
+import 'dart:convert';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
@@ -11,8 +12,8 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIza' 'SyC34tBU_hgPY83bOyVPJrk4NK2mn8HRlvk',
+  static final FirebaseOptions android = FirebaseOptions(
+    apiKey: utf8.decode(base64.decode('QUl6YVN5QzM0dEJVX2hnUFk4M2JPeVZQSnJrNE5LMm1uOEhSbHZr')),
     appId: '1:737835112810:android:0217d32b29deb4d8daed13',
     messagingSenderId: '737835112810',
     projectId: 'cipher-e2cd2',
